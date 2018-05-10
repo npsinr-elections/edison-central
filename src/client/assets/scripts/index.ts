48537dcfe807d01918e0681f2c0b4dc5f32bd523
Rishit Bansal <rishit.bansal0@gmail.com>
2018-05-11 01:36:27 +0530

    fix: elections page doesn't render wihout slash in url

--- src/client/assets/scripts/index.ts
+++ src/client/assets/scripts/index.ts
@@ -4,39 +4,36 @@
  */
 
 $(() => {
-  const navlinks = $(".nav-link") as JQuery<HTMLAnchorElement>;
-  const mainContent = $("main") as JQuery<HTMLMainElement>;
+    const navlinks = $(".nav-link") as JQuery<HTMLAnchorElement>;
+    const mainContent = $("main") as JQuery<HTMLMainElement>;
 
-  // Load the page navigated to initally on the URL bar
-  let currentPath: string = window.location.pathname;
-  if (currentPath === "/") {
-    currentPath = "/elections";
-  }
-  loadPage(mainContent, currentPath);
-  $("a[href=\"" + currentPath + "\"]").addClass("active");
+    // Load the page navigated to initally on the URL bar
+    const currentPath: string = window.location.pathname;
+    loadPage(mainContent, currentPath);
+    $("a[href=\"" + currentPath + "\"]").addClass("active");
 
-  // When users press back button on browser, load previous page
-  $(window).on("popstate", () => {
-    loadPage(mainContent, window.location.pathname);
-  });
+    // When users press back button on browser, load previous page
+    $(window).on("popstate", () => {
+        loadPage(mainContent, window.location.pathname);
+    });
 
-  // Event handlers for navigation links onclick
-  navlinks.click((e) => {
-    const link = $(e.currentTarget).attr("href");
-    if (link === "/users/logout") {
-      window.location.href = link;
-      return;
-    }
-    // Push link in browser history
-    window.history.pushState("", "", link);
+    // Event handlers for navigation links onclick
+    navlinks.click((e) => {
+        const link = $(e.currentTarget).attr("href");
+        if (link === "/users/logout") {
+            window.location.href = link;
+            return;
+        }
+        // Push link in browser history
+        window.history.pushState("", "", link);
 
-    // Set  only current navlink as "active" on display
-    navlinks.removeClass("active");
-    $(e.currentTarget).addClass("active");
+        // Set  only current navlink as "active" on display
+        navlinks.removeClass("active");
+        $(e.currentTarget).addClass("active");
 
-    loadPage(mainContent, link);
-    e.preventDefault();
-  });
+        loadPage(mainContent, link);
+        e.preventDefault();
+    });
 });
 
 /**
@@ -45,9 +42,12 @@ $(() => {
  * @param  {string} pagePath path for requested page
  */
 function loadPage(mainContent: JQuery<HTMLMainElement>, pagePath: string) {
-  mainContent.hide("fast", () => {
-    mainContent.load("/pages" + pagePath, () => {
-      mainContent.show("fast");
+    if (pagePath === "/" || pagePath === "") {
+        pagePath = "/elections";
+    }
+    mainContent.hide("fast", () => {
+        mainContent.load("/pages" + pagePath, () => {
+            mainContent.show("fast");
+        });
     });
-  });
 }
