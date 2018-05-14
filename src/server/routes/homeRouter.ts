45e19b5883a388cc3aef8a23c1c01df32d4ac220
Muktaka Joshipura <muktaka2000@gmail.com>
2018-05-14 23:56:40 +0530

    feat: Move to server templating and to a Bootstrap-based design

--- src/server/routes/homeRouter.ts
+++ src/server/routes/homeRouter.ts
@@ -12,7 +12,7 @@ import express = require("express");
 export const router = express.Router();
 
 router.use((_REQ, res, next) => {
-  res.setHeader("Cache-Control", "no-cache,no-store, max-age=0," +
+  res.setHeader("Cache-Control", "no-cache,no-store,max-age=0," +
     "must-revalidate");
   next();
 });
@@ -26,11 +26,24 @@ router.get("/pages/:pageName", (req, res) => {
   res.render(req.params.pageName + ".html");
 });
 
+const pageNames: Map<string, string> = new Map(
+  [["elections", "Elections"], ["settings", "Settings"]]);
+
+const navlinks: Array<[string, string, string]> = [
+  ["/elections", "Elections", "fab fa-dropbox"],
+  ["/settings", "Settings", "fas fa-cog"],
+  ["/users/logout", "Log Out", "fas fa-sign-out-alt"]];
+
 /**
  * Route to display various app pages after logged in
  * @name get/:pageName
  * @function
  */
-router.get("/:pageName(elections|results|settings)?", (_REQ, res) => {
-  res.render("index.html");
+router.get("/:pageName(elections|settings)?", (req, res) => {
+  res.render("index.html", {
+    appName: "edison-central",
+    pageTitle: pageNames.get(req.params.pageName),
+    currentURL: `/${req.params.pageName}`,
+    navlinks: navlinks
+  });
 });
