07657d6a74e8b80bb6a5090da4f1b263caa70514
Muktaka Joshipura <muktaka2000@gmail.com>
2018-05-23 23:59:23 +0530

    feat: Style, add color, image and group inputs

--- src/client/assets/scripts/electionEdit.ts
+++ src/client/assets/scripts/electionEdit.ts
@@ -1,11 +1,17 @@
 $(() => {
-  const electionForm = $("#election-form") as JQuery<HTMLFormElement>;
+  /**
+   * Submit the form data to the server
+   */
+  $("#election-form").submit((e: JQuery.Event<HTMLFormElement, null>) => {
+    const electionForm = $(e.target);
+    const data = new FormData(e.target);
 
-  electionForm.submit((e: JQuery.Event<HTMLFormElement, null>) => {
-    console.log(electionForm.serialize());
     $.ajax({
-      type: "PUT",
-      data: electionForm.serialize(),
+      method: "PUT",
+      data: data,
+      cache: false,
+      processData: false,
+      contentType: "multipart/form-data",
       success: (res) => {
         console.log(res);
       },
@@ -14,4 +20,28 @@ $(() => {
 
     e.preventDefault();
   });
+
+  /**
+   * Change the image when an image is uploaded
+   */
+  $("#election-image-input").change(
+    (e: JQuery.Event<HTMLInputElement, null>) => {
+      const imageInput = e.target;
+      const imagePreview = $("#election-uploaded-image");
+      const reader = new FileReader();
+
+      reader.onload = (readEvent) => {
+        imagePreview.attr("src", readEvent.target.result);
+      };
+
+      if (imageInput.files && imageInput.files[0]) {
+        if (imageInput.files.length === 1) {
+          reader.readAsDataURL(imageInput.files[0]);
+        } else {
+          imagePreview.attr("src", "#");
+          imagePreview.attr("alt", "Too many images.");
+        }
+      }
+    }
+  );
 });
