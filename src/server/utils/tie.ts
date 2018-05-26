d34b9374287440356ad4fd8ec826b835bd5aee5e
thebongy <rishit.bansal0@gmail.com>
2018-05-26 19:36:23 +0530

    feat: Detect ties in polls

--- src/server/utils/tie.ts
+++ src/server/utils/tie.ts
@@ -1,36 +1,26 @@
-let json={
-    "polls":[{
-        "name":"Challengers Vice Captain",
-        "foreColor":"#FFF",
-        "backColor":"#F00",
-        "candidates":[
-            {
-                "name":"Superman",
-                "votes":100,
-                "imageURL":"path/to/picture.jpg"
-            }
-        ]
-    }
-]
+import { Candidate, Poll } from "../model/elections";
+
+export interface Tie {
+  pollName: string;
+  candidates: string[];
 }
-checkVotes("Challengers Vice Captain");
-function checkVotes(office: string) {
-  let check = 0;
-  for (const i of json.polls) {
-    if (i.name === office) {
-      let MAXVOTES = i.candidates[0].votes;
-      for (const j of i.candidates) {
-        if (j.votes > MAXVOTES) {
-          MAXVOTES = j.votes;
-          }}
-      for (const k of i.candidates) {
-        if (k.votes === MAXVOTES) {
-          check += 1;
-            }
-        }
-      if (check < 1) {
-        check = 1;//placeholder
-      }
-      }
+
+export function checkforTies(poll: Poll): string[] {
+  let candidates: Candidate[] = [];
+  let ties: string[] = [];
+  for (const candidate of poll.candidates) {
+    if (candidates.length === 0 || candidates[0].votes === candidate.votes) {
+      candidates.push(candidate);
+      ties.push(candidate.name);
+    } else if (candidates[0].votes < candidate.votes) {
+      candidates = [candidate];
+      ties = [candidate.name];
     }
+
+    if (candidates.length === 1) {
+      return [];
+    }
+
+    return ties;
   }
+}
